c05c7d58a06143712ae0cf86bbecd508785cc2a8
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-10-02 13:36:21 +0300

    Adjusted flex and heights of few shared components

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,11 +3,11 @@ import ThemeToggle from "../modules/theme/ThemeToggle";
 import AuthImage from "../assets/auth-image.png";
 const AuthLayout = () => {
   return (
-    <div className="relative flex gap-10 px-10 py-10 justify-center items-center bg-primaryLight dark:bg-primaryDark text-primaryDark dark:text-primaryLight min-h-screen">
+    <div className="relative flex gap-10 px-10 py-10 justify-center items-start bg-primaryLight dark:bg-primaryDark text-primaryDark dark:text-primaryLight min-h-screen">
       <span className="absolute top-3 right-5">
         <ThemeToggle />
       </span>
-      <div className="w-7/12">
+      <div className="w-7/12 h-full">
         <Outlet />
       </div>
       <div className="auth-image-container h-full w-5/12">
