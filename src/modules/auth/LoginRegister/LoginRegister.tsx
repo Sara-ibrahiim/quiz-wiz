c2de156b0502e92f901e5cf3db550016b80d0814
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-10-02 12:19:04 +0300

    adjusted logo

--- src/modules/auth/LoginRegister/LoginRegister.tsx
+++ src/modules/auth/LoginRegister/LoginRegister.tsx
@@ -1,8 +1,19 @@
 import AuthButton from "../../../components/AuthButton";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
 const LoginRegister = () => {
   return (
-    <div>
+    <div className="login-register-container">
+      <div className="logo dark:text-primaryLight  text-primaryDark flex items-center">
+        <span className="text-4xl">
+          <IoIosCloseCircleOutline />
+        </span>
+        <span className="text-4xl">
+          <IoCheckmarkCircleOutline />
+        </span>
+        <span className="text-xl">| Quizwiz</span>
+      </div>
       <AuthButton title="Sign In" />
     </div>
   );
