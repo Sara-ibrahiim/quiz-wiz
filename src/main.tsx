ef0983c05a03b4f0e287a8f4d423d4a803a21121
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-10-03 12:37:28 +0300

    finished Login/Register Design/Inputs/Api Integeration

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import App from "./App.tsx";
 import "./index.css";
 import { store } from "./store/store.ts";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <StrictMode>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000} // Toast automatically closes after 5 seconds
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored" // Can also use "dark" or "light" if you prefer
+      />
     </StrictMode>
   </Provider>
 );
--- src/modules/auth/LoginRegister/LoginRegister.tsx
+++ src/modules/auth/LoginRegister/LoginRegister.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AuthLogo from "../../../components/AuthLogo";
 import AuthTitle from "./AuthTitle";
 import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 const LoginRegister = () => {
   const [mode, setMode] = useState("Login");
@@ -10,7 +11,7 @@ const LoginRegister = () => {
       <AuthLogo />
       <AuthTitle mode={mode} setMode={setMode} />
       {mode === "Login" && <LoginForm />}
-      {/* {mode === "Register" && <LoginForm />} */}
+      {mode === "Register" && <RegisterForm setMode={setMode} />}
     </div>
   );
 };
