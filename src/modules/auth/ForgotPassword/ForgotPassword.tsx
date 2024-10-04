9eb14500254ba087531d07c60d3f370af0efe170
AiaElprince <166065523+AiaElprince@users.noreply.github.com>
2024-10-04 11:04:39 +0300

    working in forgetPassword

--- src/modules/auth/ForgotPassword/ForgotPassword.tsx
+++ src/modules/auth/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import AuthLogo from "../../../components/AuthLogo";
+import AuthTitle from "../LoginRegister/AuthTitle";
+import AuthButton from "./../../../components/AuthButton";
+
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  const [mode, setMode] = useState("forgot");
+  return;
+  <div className="forget-password-container h-full flex flex-col justify-between ">
+    <AuthLogo />
+    <div className="pt-10">
+      <AuthTitle mode={mode} />
+    </div>
+    <AuthButton title="Forget Password" />
+  </div>;
 };
 
 export default ForgotPassword;
