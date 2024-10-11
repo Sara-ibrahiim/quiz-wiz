08db40bcb53e25da9b9e9504bbcf5cf0c5172eae
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-10-11 21:49:54 +0300

    Adjusted few tweaks when merged 2 pull requests

--- src/constants/End-points.ts
+++ src/constants/End-points.ts
@@ -19,26 +19,18 @@ export const Question_URls = {
   create: `${Base_Ques}`,
   delete: (id: string) => `${Base_Ques}/${id}`,
 };
-export const Auth_URls ={
-    login : `${Base_Auth}/login`,
-    register : `${Base_Auth}/register`,
-    forgetPassword: `${Base_Auth}/forgot-password`,
-    resetPassword: `${Base_Auth}/reset-password`,
-    changePassword : `${Base_Auth}/change-password`,
-    logout: `${Base_Auth}/logout`,
-}
 
-const Base_Groups= `${Base_Url}/group`;
+const Base_Groups = `${Base_Url}/group`;
 
-export const Groups_URls ={
-    getGroupsList : `${Base_Groups}`,
-    addGroup : `${Base_Groups}`,
-    updateGroup : (id: string): string => `${Base_Groups}/${id}`,
-    deleteGroup : (id: string): string => `${Base_Groups}/${id}`,
-}
+export const Groups_URls = {
+  getGroupsList: `${Base_Groups}`,
+  addGroup: `${Base_Groups}`,
+  updateGroup: (id: string): string => `${Base_Groups}/${id}`,
+  deleteGroup: (id: string): string => `${Base_Groups}/${id}`,
+};
 
-const Base_Students= `${Base_Url}/student`;
+const Base_Students = `${Base_Url}/student`;
 
-export const Students_URls ={
-    getStudentsList : `${Base_Students}`,
-}
+export const Students_URls = {
+  getStudentsList: `${Base_Students}`,
+};
