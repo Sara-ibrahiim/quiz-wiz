0b9a2c6452bcb0a9ae3552f4d19d0ff293d15ccc
youssef4nasser <youssef4nasser@gmail.com>
2024-10-16 15:22:34 +0300

    Remove StrictMode from main.tsx because it duplicates a tost message

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { StrictMode } from "react";
 import { ToastContainer } from "react-toastify";
 import { createRoot } from "react-dom/client";
 import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <StrictMode>
       <App />
       <ToastContainer
         position="top-right"
@@ -24,6 +22,5 @@ createRoot(document.getElementById("root")!).render(
         pauseOnHover
         theme="colored"
       />
-    </StrictMode>
   </Provider>
 );
