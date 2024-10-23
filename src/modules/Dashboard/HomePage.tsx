e9d24939fd3f31b33a3a00be555c2b66451e06c5
Sara-ibrahiim <sou.cute94@gmail.com>
2024-10-24 00:02:28 +0300

    Db

--- src/modules/Dashboard/HomePage.tsx
+++ src/modules/Dashboard/HomePage.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+
 import TopStudent from './TopStudent'
+import UpComingQuiz from './UpComingQuiz'
 
 export default function HomePage() {
   return (
     <>
 
-    <div className='flex p-4  grid  sm:grid-cols-1 xs:grid-cols-1 md:grid-cols-12 w-full'>
+    <div className='flex p-4  grid  sm:grid-cols-1 xs:grid-cols-1 gap-5 md:grid-cols-12 w-full'>
     <div className='md:col-span-7'>
-<h1>Quiz</h1>
+<div className='border-[1px] border-[#ECECEC] rounded px-2 pb-4 pt-3  w-full'>
+<UpComingQuiz/>
+</div>
       </div> 
       <div className='md:col-span-5 xs:py-2 sm:py-2'>
-       <div className=' border-[1px] border-[#ECECEC] rounded p-2 w-full'>
+       <div className=' border-[1px] border-[#ECECEC] rounded px-2 pb-4 pt-3 w-full'>
 <TopStudent/>
        </div>
       </div>
